5407aa574fb0ef48d2377d82e2e4b813e21d9f6a
ahbpp <alexeykarpov@MacBook-Air-Alexey.local>
2017-03-21 17:34:09 +0300

    basic

---
+++ Rational/Rational.cpp
@@ -0,0 +1,9 @@
+//
+//  Rational.cpp
+//  Rational
+//
+//  Created by Alexey Karpov on 21.03.17.
+//  Copyright © 2017 Alexey Karpov. All rights reserved.
+//
+
+#include "Rational.hpp"
---
+++ Rational/Rational.hpp
@@ -0,0 +1,14 @@
+//
+//  Rational.hpp
+//  Rational
+//
+//  Created by Alexey Karpov on 21.03.17.
+//  Copyright © 2017 Alexey Karpov. All rights reserved.
+//
+
+#ifndef Rational_hpp
+#define Rational_hpp
+
+#include <stdio.h>
+
+#endif /* Rational_hpp */
